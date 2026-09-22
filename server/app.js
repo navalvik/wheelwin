a74bc446f43d99133e529e487965352d5808d228
navalvik <57067137+navalvik@users.noreply.github.com>
2026-09-22 06:57:08 +0300

    cleanup: remove obsolete escrow mode wiring from Room Wallet router

--- server/app.js
+++ server/app.js
@@ -1491,8 +1491,7 @@ class WheelWinApplication {
         this._roomWalletSettlementRouter = composeRoomWalletSettlementRouter({
             tonService: this._services?.tonService ?? null,
             logger: this._logger,
-            env: process.env,
-            gameEscrowMode: this._tonConfig?.gameEscrowMode ?? null
+            env: process.env
         });
 
         this._logger.startupLine(
