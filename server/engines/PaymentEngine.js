e048018d851d709547af852b1a1f9e171f0d616f
navalvik <57067137+navalvik@users.noreply.github.com>
2026-09-22 06:24:00 +0300

    cleanup: remove legacy GameContract wiring from PaymentEngine

--- server/engines/PaymentEngine.js
+++ server/engines/PaymentEngine.js
@@ -14,7 +14,6 @@ export class PaymentEngine {
         winnerEngine,
         configurationEngine,
         gameCatalog,
-        gameContractManager = null,
         telegramWalletAdapter,
         metricsService = null
     }) {
@@ -29,8 +28,6 @@ export class PaymentEngine {
 
         this._gameCatalog = gameCatalog;
 
-        this._gameContractManager = gameContractManager;
-
         this._telegramWalletAdapter = telegramWalletAdapter;
 
         this._metricsService = metricsService;
@@ -44,12 +41,6 @@ export class PaymentEngine {
         this._infrastructureHandlers = [];
 
         this._initialized = false;
-
-    }
-
-    setGameContractManager(gameContractManager) {
-
-        this._gameContractManager = gameContractManager ?? null;
 
     }
 
@@ -357,7 +348,6 @@ export class PaymentEngine {
     _resolvePaymentRules(gameId) {
 
         const resolved = resolveGameFinancialRules(gameId, {
-            gameContractManager: this._gameContractManager,
             configurationEngine: this._configurationEngine,
             gameCatalog: this._gameCatalog
         });
